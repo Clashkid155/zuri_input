aacd4628f619e7e3fbc17021f19b1dae59e83c51
Clashkid155 <frankdiss9@gmail.com>
2021-08-17 20:45:12 +0100

    Init commit

---
+++ lib/main.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/svg.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.grey.withOpacity(0.5),
+    systemNavigationBarColor: Colors.grey.withOpacity(0.5),
+  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Zuri Task',
+      theme: ThemeData(
+        fontFamily: 'Averta',
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with AutomaticKeepAliveClientMixin {
+  TextEditingController controller = TextEditingController();
+  var text = '';
+  final String svg = 'images/Zuri.svg';
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        backgroundColor: Colors.grey.withOpacity(0.5),
+        elevation: 0,
+        title: SvgPicture.asset(svg),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Center(
+          child: Container(
+            padding: EdgeInsets.all(15),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.grey.withOpacity(0.5),
+                boxShadow: [BoxShadow(color: Colors.grey.withOpacity(0.5))]),
+            height: MediaQuery.of(context).size.height / 4,
+            width: MediaQuery.of(context).size.height / 2.4,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  text,
+                  style: TextStyle(fontSize: 20),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                TextField(
+                  onChanged: (value) {
+                    setState(() {
+                      text = value;
+                    });
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Input text',
+                    focusColor: Colors.transparent,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide(width: 5)),
+                  ),
+                  controller: controller,
+                ),
+              ],
+            ),
+          ),
+        ),
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.grey.withOpacity(0.5),
+        ),
+      ),
+    );
+  }
+
+  @override
+  bool get wantKeepAlive => true;
+}
